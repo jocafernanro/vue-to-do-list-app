93d658fd2df01531e482b6dcc85bfbb926ce1393
Jose Carlos Fernandez Rodriguez <jocafernanro@gmail.com>
2020-04-25 23:24:56 +0200

    Init config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :root {
+    --me: red;
+  }
+
+  @media screen and (max-width: 750px) {
+    :root {
+      --me: red;
+
+    }
+  }
+
+  @media screen and (max-width: 375px) {
+    :root {
+      --me: red;
+
+    }
+  }
+
+  body{
+    font-family: neue-haas-grotesk-display, sans-serif;
+  }
 </style>
